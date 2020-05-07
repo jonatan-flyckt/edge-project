118cdd082f2ad73339442ebe8b7454e5e151065b
maggemkay <magge.96@hotmail.com>
2020-05-07 15:50:43 +0200

    Adds landmarks class.

--- code/app/src/main/java/com/google/ar/core/examples/java/sharedcamera/SharedCameraActivity.java
+++ code/app/src/main/java/com/google/ar/core/examples/java/sharedcamera/SharedCameraActivity.java
@@ -197,6 +197,12 @@ public class SharedCameraActivity extends AppCompatActivity
     }
   }
 
+  // Egna saker --------------------------------------------------------------------------------
+
+  private final LandmarksHelper landmarksHelper = new LandmarksHelper();
+
+
+
   // Camera device state callback.
   private final CameraDevice.StateCallback cameraDeviceCallback =
       new CameraDevice.StateCallback() {
@@ -816,9 +822,6 @@ public class SharedCameraActivity extends AppCompatActivity
     // in createCameraPreviewSession() via sharedSession.setCameraTextureName(…).
     isGlAttached = true;
 
-    // Handle screen tap.
-    handleTap(frame, camera); // <----------------------------------------------------------
-
     // If frame is ready, render camera preview image to the GL surface.
     backgroundRenderer.draw(frame); // <----------------------------------------------------------
 
@@ -868,9 +871,6 @@ public class SharedCameraActivity extends AppCompatActivity
     planeRenderer.drawPlanes(
         sharedSession.getAllTrackables(Plane.class), camera.getDisplayOrientedPose(), projmtx);
 
-// Set the camera position (View matrix)
-//    Matrix.setLookAtM(viewmtx, 0, 0f, 0f, 2f, 0f, 0f, 0f, 0.0f, 0.0f, 0.0f);
-
     // Visualize anchors created by touch.
     float scaleFactor = 1.0f;
     for (ColoredAnchor coloredAnchor : anchors) {
@@ -886,50 +886,6 @@ public class SharedCameraActivity extends AppCompatActivity
       virtualObjectShadow.updateModelMatrix(anchorMatrix, scaleFactor);
       virtualObject.draw(viewmtx, projmtx, colorCorrectionRgba, coloredAnchor.color);
       virtualObjectShadow.draw(viewmtx, projmtx, colorCorrectionRgba, coloredAnchor.color);
-    }
-  }
-
-  // Handle only one tap per frame, as taps are usually low frequency compared to frame rate.
-  private void handleTap(Frame frame, Camera camera) {
-    MotionEvent tap = tapHelper.poll();
-    if (tap != null && camera.getTrackingState() == TrackingState.TRACKING) {
-      for (HitResult hit : frame.hitTest(tap)) {
-        // Check if any plane was hit, and if it was hit inside the plane polygon
-        Trackable trackable = hit.getTrackable();
-        // Creates an anchor if a plane or an oriented point was hit.
-        if ((trackable instanceof Plane
-                && ((Plane) trackable).isPoseInPolygon(hit.getHitPose())
-                && (PlaneRenderer.calculateDistanceToPlane(hit.getHitPose(), camera.getPose()) > 0))
-            || (trackable instanceof Point
-                && ((Point) trackable).getOrientationMode()
-                    == OrientationMode.ESTIMATED_SURFACE_NORMAL)) {
-          // Hits are sorted by depth. Consider only closest hit on a plane or oriented point.
-          // Cap the number of objects created. This avoids overloading both the
-          // rendering system and ARCore.
-          if (anchors.size() >= 20) {
-            anchors.get(0).anchor.detach();
-            anchors.remove(0);
-          }
-
-          // Assign a color to the object for rendering based on the trackable type
-          // this anchor attached to. For AR_TRACKABLE_POINT, it's blue color, and
-          // for AR_TRACKABLE_PLANE, it's green color.
-          float[] objColor;
-          if (trackable instanceof Point) {
-            objColor = new float[] {66.0f, 133.0f, 244.0f, 255.0f};
-          } else if (trackable instanceof Plane) {
-            objColor = new float[] {139.0f, 195.0f, 74.0f, 255.0f};
-          } else {
-            objColor = DEFAULT_COLOR;
-          }
-
-          // Adding an Anchor tells ARCore that it should track this position in
-          // space. This anchor is created on the Plane to place the 3D model
-          // in the correct position relative both to the world and to the plane.
-          anchors.add(new ColoredAnchor(hit.createAnchor(), objColor));
-          break;
-        }
-      }
     }
   }
 
